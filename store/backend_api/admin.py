9109cdee900478c54132877b6ee56e46f210f7a0
gulyaev <gulyaev.gvs@gmail.com>
2024-12-26 16:49:10 +0500

    Register Model SchhoolSubject in admin panel

--- store/backend_api/admin.py
+++ store/backend_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from backend_api.models import SchoolSubject
+
+admin.site.register(SchoolSubject)
+
--- store/backend_api/models.py
+++ store/backend_api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class SchoolSubject(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return f'Предмет: {self.title}'
