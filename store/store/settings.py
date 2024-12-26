182241a8c84d0e527f6ab7cc431c540414dd1852
gulyaev <gulyaev.gvs@gmail.com>
2024-12-26 20:16:18 +0500

    fix commit: settings Allowed hosts all

--- store/store/settings.py
+++ store/store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jr^$x5+%%t-y&7%3c6^puaf^5*cs@&1!^ike$&hg#xym05d@f^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
